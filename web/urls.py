c5c3de1e70385ab101eee1ed0e1f6eeb0860172b
unknown <ilmancc@gmail.com>
2024-10-15 01:37:23 -0700

    Added submissions page

--- web/urls.py
+++ web/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('submissions',views.submissions,name='submissions'),
     
 
 ]
--- web/views.py
+++ web/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def submissions(request):
+    return render(request, 'submissions.html')
