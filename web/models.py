98a3e508bf6f631afb64ae7352fde7146448f186
unknown <ilmancc@gmail.com>
2024-10-30 16:09:35 -0700

     Changes to be committed:

--- web/models.py
+++ web/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import re
-from ckeditor.fields import RichTextField
+# from ckeditor.fields import RichTextField
 
 class Information(models.Model):
     pdf = models.FileField(blank=True, null=True)
