cdf678ef5ebcac85b1a8058a275fbe59c594e565
kashifmanzerSMI <kashif@settlemint.com>
2025-10-25 14:46:59 -0700

    feat: changes for seet 2026

--- ICSE_ET/settings.py
+++ ICSE_ET/settings.py
@@ -32,8 +32,8 @@ STRIPE_ENDPOINT_SECRET= env('STRIPE_ENDPOINT_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["seet25.sw-conf.com","sw-conf.com"]
-#ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+#ALLOWED_HOSTS = ["seet25.sw-conf.com","sw-conf.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
